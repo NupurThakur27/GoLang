df360b17dfcd005991d96bcc13280dea09b7aaf7
NupurThakur27 <nupur.s.thakur@gmail.com>
2020-05-17 14:53:13 +0530

    Check

--- nupurthakur27/RemoteState/GoLang/main.go
+++ nupurthakur27/RemoteState/GoLang/main.go
@@ -1,5 +1,4 @@
 package main
-
 import "fmt"
 func main() {
 	Hello()
@@ -8,6 +7,43 @@ func main() {
 	fmt.Println("Do you want to know what 2+3 is? It's",add(2,3))
 	fmt.Println("It's Boolean time, is it true or false? ",Boolean())
 	showvar()
-
-
+	fmt.Println("Let's play with your name now.")
+	fmt.Println(swap("Nupur","Thakur"))
+	fmt.Println("As we human have caste,religions to discriminate similarly programming Language have Data types")
+	VarType()
+	fmt.Println("Wait till 10 to proceed further")
+	Basic_loop()
+	fmt.Println("Looks like you have to wait more")
+	WO_Init_Post()
+	fmt.Println("Time to play with for loop, or the only loop in Go")
+	pattern1()
+	Always()
+	//IF-ELSE
+	Voting(21)
+	DayoftheWeek(7)
+	//Defer example
+	reverse()
+	demoptr()
+	demostruct()
+	Printarray()
+	DemoSlice()
+	Make_slice()
+	MultiD_Slice()
+	Sort_Slice()
+	Demo_Map()
+	Map_Operations()
+	//Map Exercise
+	f := fibonacci()
+	fmt.Println("Fibonacci Series is:")
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+	//Method Example
+	book := Book{
+		bname: "Tarkash",
+		author: "Javed Akthar",
+		bno: 1,
+		ispn: 1235,
+	}
+	book.Demo_Method()
 }
